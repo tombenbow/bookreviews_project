0d201ede0ed7a304ee2cc7dcc0594f0f365fca77
Tom <tombenbow@googlemail.com>
2020-10-18 23:46:00 +0100

    updated delete to just use reqparams

--- models/model.js
+++ models/model.js
@@ -9,10 +9,10 @@ const fetchUsers = () => {
     .then((allUsers) => {
       return { all_users: allUsers };
     })
-    .catch(error => {
-      console.log(error)
-      return error
-    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
 };
 
 const fetchTopics = () => {
@@ -22,10 +22,10 @@ const fetchTopics = () => {
     .then((allTopics) => {
       return { all_topics: allTopics };
     })
-    .catch(error => {
-      console.log(error)
-      return error
-    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
 };
 
 const fetchThatUser = (id) => {
@@ -42,10 +42,10 @@ const fetchThatUser = (id) => {
     .then((thatUser) => {
       return thatUser.length > 0 ? { requested_user: thatUser } : {};
     })
-    .catch(error => {
-      console.log(error)
-      return error
-    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
 };
 
 const fetchThatReview = (id) => {
@@ -57,44 +57,49 @@ const fetchThatReview = (id) => {
     .then((requestedBookReview) => {
       return requestedBookReview.length > 0 ? { requestedBookReview } : {};
     })
-    .catch(error => {
-      console.log(error)
-      return error
-    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
 };
 
 const fetchAllReviews = (sort_by, order, username, topic) => {
-  return new Promise ((resolve, reject) => {
-  return connection
-    .select("book_reviews.*")
-    .from("book_reviews")
-    .orderBy(sort_by || "review_id", order || "desc")
-    .modify(reviews => {
-      if (topic) {
-        return reviews.where("topic_name", "=", topic)
-      }
-      if (username) {
-        return reviews.where("username", "=", username)
-      }
-    })
-    .then((allBookReviews) => {
-      if (allBookReviews.length > 0) {
-        resolve ({ all_bookreviews: allBookReviews });
-      }
-      else {
-        reject({status: 404})
-      }
-    })
-    .catch(error => {
-      console.log(error)
-      return error
-    })
-  })
+  return new Promise((resolve, reject) => {
+    return connection
+      .select("book_reviews.*")
+      .from("book_reviews")
+      .orderBy(sort_by || "review_id", order || "desc")
+      .modify((reviews) => {
+        if (topic) {
+          return reviews.where("topic_name", "=", topic);
+        }
+        if (username) {
+          return reviews.where("username", "=", username);
+        }
+      })
+      .then((allBookReviews) => {
+        if (allBookReviews.length > 0) {
+          resolve({ all_bookreviews: allBookReviews });
+        } else {
+          reject({ status: 404 });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  });
 };
 
 const fetchThatReviewsComments = (id, sort_by, order) => {
   return connection
-    .select("comments.comment_key", "comments.body", "comments.username", "comments.comment_votes", "users.profile_picture")
+    .select(
+      "comments.comment_key",
+      "comments.body",
+      "comments.username",
+      "comments.comment_votes",
+      "users.profile_picture"
+    )
     .from("comments")
     .where("review_id", "=", id)
     .join("users", "users.username", "=", "comments.username")
@@ -102,187 +107,162 @@ const fetchThatReviewsComments = (id, sort_by, order) => {
     .then((theReviewsComments) => {
       return { theReviewsComments };
     })
-    .catch(error => {
-      console.log(error)
-      return error
-    })
+    .catch((error) => {
+      console.log(error);
+      return error;
+    });
 };
 
 const newComment = (reqParams, reqBody) => {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (reqBody.hasOwnProperty("body") && reqBody.hasOwnProperty("username")) {
       return connection
-      .select("username")
-      .from("users")
-      .where("username", "=", reqBody.username)
-      .then(usernames => {
-        if (usernames.length > 0) {
-          return connection
-          .select("review_id")
-          .from("book_reviews")
-          .where("review_id", "=", reqParams.review_id)
-          .then(reviewId => {
-            if (reviewId.length > 0) {
-              reqBody.review_id = parseInt(reqParams.review_id);
-              reqBody.comment_votes = 0;
-              return connection
-              .insert(reqBody)
-              .into('comments')
-              .returning('*')
-              .then(insertedComment => {
-                resolve(insertedComment)
-              })
-            }
-            else {
-              reject({status: 404})
-            }
-          })
-        }
-        else {
-          reject({status: 400})
-        }
-      })
+        .select("username")
+        .from("users")
+        .where("username", "=", reqBody.username)
+        .then((usernames) => {
+          if (usernames.length > 0) {
+            return connection
+              .select("review_id")
+              .from("book_reviews")
+              .where("review_id", "=", reqParams.review_id)
+              .then((reviewId) => {
+                if (reviewId.length > 0) {
+                  reqBody.review_id = parseInt(reqParams.review_id);
+                  reqBody.comment_votes = 0;
+                  return connection
+                    .insert(reqBody)
+                    .into("comments")
+                    .returning("*")
+                    .then((insertedComment) => {
+                      resolve(insertedComment);
+                    });
+                } else {
+                  reject({ status: 404 });
+                }
+              });
+          } else {
+            reject({ status: 400 });
+          }
+        });
+    } else {
+      reject({ status: 400 });
     }
-    else {
-      reject({status: 400})
-    }
-  })
+  });
 };
 
 const fetchEndpoints = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile(path.join(__dirname,"../api_descriptor.json"), (err, data) => {
-      if(err)reject(err)
-      parsedData = JSON.parse(data)
-      resolve({ available_endpoints: parsedData })
+    fs.readFile(path.join(__dirname, "../api_descriptor.json"), (err, data) => {
+      if (err) reject(err);
+      parsedData = JSON.parse(data);
+      resolve({ available_endpoints: parsedData });
+    });
+  }).catch((error) => {
+    console.log(error);
+    return error;
   });
-  })
-  .catch(error => {
-    console.log(error)
-    return error
-  })
 };
 
 const reviewVotes = (reqBody, reqParams) => {
-  return  new Promise((resolve, reject) => {
-  if (reqBody.hasOwnProperty("vote")) {
+  return new Promise((resolve, reject) => {
+    if (reqBody.hasOwnProperty("vote")) {
       return connection
-      .select("*")
-      .from("book_reviews")
-      .where("review_id", "=", reqParams.review_id)
-      .returning('*')
-      .then(review => {
-        if (review.length == 0) {
-          reject({status : 404})
-        }
-        else {
-          return review
-        }
-      })
-      .then(review => {
+        .select("*")
+        .from("book_reviews")
+        .where("review_id", "=", reqParams.review_id)
+        .returning("*")
+        .then((review) => {
+          if (review.length == 0) {
+            reject({ status: 404 });
+          } else {
+            return review;
+          }
+        })
+        .then((review) => {
           if (reqBody.vote == "up") {
             return connection("book_reviews")
-            .where("review_id", reqParams.review_id)
-            .update("review_votes", (review[0].review_votes +1))
-            .returning("*")
-            .then(review => {
-              resolve(review)
-            })
-          }
-          else if (reqBody.vote == "down") {
+              .where("review_id", reqParams.review_id)
+              .update("review_votes", review[0].review_votes + 1)
+              .returning("*")
+              .then((review) => {
+                resolve(review);
+              });
+          } else if (reqBody.vote == "down") {
             return connection("book_reviews")
-            .where("review_id", "=", reqParams.review_id)
-            .update("review_votes", (review[0].review_votes -1))
-            .returning("*")
-            .then(review => {
-              resolve(review)
-            })
+              .where("review_id", "=", reqParams.review_id)
+              .update("review_votes", review[0].review_votes - 1)
+              .returning("*")
+              .then((review) => {
+                resolve(review);
+              });
+          } else {
+            reject({ status: 400 });
           }
-          else {
-              reject({status : 400})
-          }
-      })
-  }
-  else {
-        reject({status : 400})
-  }
-})
-}
+        });
+    } else {
+      reject({ status: 400 });
+    }
+  });
+};
 
 const commentVotes = (reqBody, reqParams) => {
-  return  new Promise((resolve, reject) => {
-  if (reqBody.hasOwnProperty("vote")) {
+  return new Promise((resolve, reject) => {
+    if (reqBody.hasOwnProperty("vote")) {
       return connection
-      .select("*")
-      .from("comments")
-      .where("comment_key", "=", reqParams.comment_key)
-      .returning('*')
-      .then(comment => {
-        if (comment.length == 0) {
-          reject({status : 404})
-        }
-        else {
-          return comment
-        }
-      })
-      .then(comment => {
+        .select("*")
+        .from("comments")
+        .where("comment_key", "=", reqParams.comment_key)
+        .returning("*")
+        .then((comment) => {
+          if (comment.length == 0) {
+            reject({ status: 404 });
+          } else {
+            return comment;
+          }
+        })
+        .then((comment) => {
           if (reqBody.vote == "up") {
             return connection("comments")
-            .where("comment_key", reqParams.comment_key)
-            .update("comment_votes", (comment[0].comment_votes +1))
-            .returning("*")
-            .then(comment => {
-              resolve(comment)
-            })
-          }
-          else if (reqBody.vote == "down") {
+              .where("comment_key", reqParams.comment_key)
+              .update("comment_votes", comment[0].comment_votes + 1)
+              .returning("*")
+              .then((comment) => {
+                resolve(comment);
+              });
+          } else if (reqBody.vote == "down") {
             return connection("comments")
-            .where("comment_key", reqParams.comment_key)
-            .update("comment_votes", (comment[0].comment_votes -1))
-            .returning("*")
-            .then(comment => {
-              resolve(comment)
-            })
+              .where("comment_key", reqParams.comment_key)
+              .update("comment_votes", comment[0].comment_votes - 1)
+              .returning("*")
+              .then((comment) => {
+                resolve(comment);
+              });
+          } else {
+            reject({ status: 400 });
           }
-          else {
-              reject({status : 400})
-          }
-      })
-  }
-  else {
-        reject({status : 400})
-  }
-})
-}
+        });
+    } else {
+      reject({ status: 400 });
+    }
+  });
+};
 
 const deleteComment = (reqParams) => {
-  if (reqParams.hasOwnProperty("username")) {
-    return connection
+  return connection
     .select("*")
     .from("comments")
     .where("comment_key", "=", parseInt(reqParams.comment_key))
-    .returning('*')
-    .then(theComment => {
-      if (theComment[0].username == reqParams.username) {
-        return connection
-        .del("*")
-        .from("comments")
-        .where("comment_key", "=", reqParams.comment_key)
-        .returning('*')
-      }
-      else {
+    .del("*")
+    .then((deletedComment) => {
+      if (deletedComment === 0) {
         return []
       }
+      else {
+        return [deletedComment]
+      }
     })
-    .catch(error => {
-      console.log(error)
-      return error
-    })
-  }
-  else {
-    return []
-  }
-}
+};
 
 module.exports = {
   fetchUsers,
@@ -295,5 +275,5 @@ module.exports = {
   fetchEndpoints,
   reviewVotes,
   commentVotes,
-  deleteComment
+  deleteComment,
 };
